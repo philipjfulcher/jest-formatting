67d2d684c682c18bf414f6d1508d00ef2b718dd6
Philip Fulcher <philip@nrwl.io>
2024-09-27 13:38:37 -0600

    adding intetional test erro

--- apps/jest-formatting/src/app/app.spec.tsx
+++ apps/jest-formatting/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome jest-formatting/gi)).toBeTruthy();
+    expect(getByText(/Welcome jest-formatting intentiona fail/gi)).toBeTruthy();
   });
 });
